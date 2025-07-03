a44ab6c7ade120b22e03435fdb738e4418731811
Franklin <franklin.espinoza0226@gmail.com>
2025-07-03 10:42:59 -0500

    Revert "refactor: Actualiza la implementación del flujo RAG utilizando ChatPromptTemplate y StrOutputParser"
    This reverts commit d5e47c02ccb8bf58dcd467fab2c30ee571f94816.

--- src/langchain_rag.py
+++ src/langchain_rag.py
@@ -1,9 +1,10 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from retriever import Retriever
 import os
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_core.messages import HumanMessage
+from retriever import Retriever
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
@@ -18,9 +19,11 @@ Pregunta: {question}
 
 Respuesta:
 """
-prompt = ChatPromptTemplate.from_template(prompt_template_str)
-chain = prompt | llm | StrOutputParser()
-
+prompt_template = PromptTemplate(
+    template=prompt_template_str,
+    input_variables=["context", "question"]
+)
+llm_chain = LLMChain(llm=llm, prompt=prompt_template)
 # flujo RAG
 retriever = Retriever(
         index_path="./models/patient_cases.index",
@@ -30,5 +33,5 @@ retriever = Retriever(
 query = "Que tratamiento es mejor para pacientes con insomnio por sentir una presión constante por rendir al máximo y miedo a cometer errores??"
 contexto = retriever.search(query, top_k=10)
 inputs = {"context": contexto, "question": query}
-respuesta = chain.invoke(inputs)
-print(respuesta)
+respuesta = llm_chain.invoke(inputs)
+print(respuesta['text'])
