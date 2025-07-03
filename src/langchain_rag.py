aa23f5778c50fae3c413030681c1fd4e71bd8424
Franklin <franklin.espinoza0226@gmail.com>
2025-07-03 10:47:46 -0500

    refactor: Refactoring langchain rag, now using ChatPromptTemplate and SrtOutputParser to get the string results

--- src/langchain_rag.py
+++ src/langchain_rag.py
@@ -1,14 +1,17 @@
 import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from retriever import Retriever
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", google_api_key=api_key)
+if not api_key:
+    raise ValueError("No se encontró la GOOGLE_API_KEY en el archivo .env")
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=api_key)
+
 prompt_template_str = """
 Responde la pregunta basándote únicamente en el siguiente contexto:
 
@@ -19,19 +22,22 @@ Pregunta: {question}
 
 Respuesta:
 """
-prompt_template = PromptTemplate(
-    template=prompt_template_str,
-    input_variables=["context", "question"]
-)
-llm_chain = LLMChain(llm=llm, prompt=prompt_template)
-# flujo RAG
-retriever = Retriever(
-        index_path="./models/patient_cases.index",
-        cases_path="./models/patient_cases.pkl",
-        model_name="all-mpnet-base-v2")
+prompt = ChatPromptTemplate.from_template(prompt_template_str)
 
-query = "Que tratamiento es mejor para pacientes con insomnio por sentir una presión constante por rendir al máximo y miedo a cometer errores??"
-contexto = retriever.search(query, top_k=10)
+chain = prompt | llm | StrOutputParser()
+
+retriever = Retriever()
+query = "¿Qué es la ansiedad y cómo se relaciona con la búsqueda de empleo?"
+print(f"Pregunta del usuario: {query}\n")
+
+print("Recuperando contexto...")
+contexto = retriever.get_context(query)
+print("Contexto recuperado.\n")
+
+print("Generando respuesta con LangChain...")
 inputs = {"context": contexto, "question": query}
-respuesta = llm_chain.invoke(inputs)
-print(respuesta['text'])
+
+respuesta = chain.invoke(inputs)
+
+print("--- Respuesta Final ---")
+print(respuesta)
